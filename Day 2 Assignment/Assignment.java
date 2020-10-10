7dc2caa45a10c247644a4949ad565773d873b756
Divya Lakshmi V <46861059+Divyah007@users.noreply.github.com>
2020-10-10 20:51:01 +0530

    Add files via upload

---
+++ Day 2 Assignment/Assignment.java
@@ -0,0 +1,20 @@
+package com.Day_two;
+
+public class Assignment {
+
+    public static void main(String[] args) {
+    Employee e1=new Employee();
+      e1.name="Divya";
+      e1.age=22;
+	  e1.city="Coimbatore";
+	  e1.display();
+
+	System.out.println();
+
+	Employee e2=new Employee();
+		e2.name="Prabhas";
+		e2.age=41;
+		e2.city="Hyderabad";
+		e2.display();
+    }
+}
